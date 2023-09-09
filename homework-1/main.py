583454983576be65d0a4b8296bf9a92db6bcfe10
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-09-08 20:01:55 -0700

    program 1 fix

--- homework-1/main.py
+++ homework-1/main.py
@@ -9,17 +9,17 @@
 - The minimum (the smaller of the two)
 """
 
+
 def program1(num1, num2):
     print("Sum: %s" % str(num1 + num2))
 
-    # Assuming this is integer division since the prompt explicitly asks for integers 
-    print("Difference: %s" %  str(num1 - num2))
+    print("Difference: %s" % str(num1 - num2))
     print("Product: %s" % str(num1 * num2))
 
-    # Same here
-    print("Average: %s" % str((num1+num2)/2))
+    # Assuming integer division here since it explicitly asks for integer input in the prompt
+    print("Average: %s" % str((num1 + num2) / 2))
 
-    print("Distance: %s" % str(abs(num1-num2)))
+    print("Distance: %s" % str(abs(num1 - num2)))
     print("Max: %s" % str(max(num1, num2)))
     print("Min: %s" % str(min(num1, num2)))
 
@@ -39,9 +39,12 @@ hybrid and a comparable car from the Web. Run your program twice, using today’
 gas price and 15,000 miles per year.
 """
 
-def program2(cost, milesDriven, gasPrice, milesPerGallon, resaleValue = None, resale=False):
+
+def program2(
+    cost, milesDriven, gasPrice, milesPerGallon, resaleValue=None, resale=False
+):
     gasCost = (milesDriven / milesPerGallon) * gasPrice * 5
-    
+
     if resale:
         changeInValue = cost - resaleValue
         total = changeInValue + gasCost
@@ -50,11 +53,13 @@ def program2(cost, milesDriven, gasPrice, milesPerGallon, resaleValue = None, re
 
     return total
 
+
 """
 3. Write a program that reads two times in military format (0900, 1730) and prints the
 number of hours and minutes between the two times. Here is a sample run. User input
 is in color.
 """
+
 
 def program3(time1, time2):
     timeDifference = str(abs(time2 - time1))
@@ -65,13 +70,10 @@ def program3(time1, time2):
         print("%s hours %s minutes" % (timeDifference[:2], timeDifference[2:]))
 
 
-
-
 if __name__ == "__main__":
     # Program 1
     num1 = int(input("Enter an integer:\n"))
     num2 = int(input("Enter another integer:\n"))
-
 
     program1(num1, num2)
 
@@ -82,17 +84,22 @@ if __name__ == "__main__":
     gasPrice = float(input("Enter the current cost per gallon of gas:\n"))
     milesPerGallon = float(input("Enter the miles per gallon given by this vehicle:\n"))
     resaleValue = int(input("Enter the resale value of this vehicle after 5 years:\n"))
-    
-    baseTotal = program2(cost, MILES_DRIVEN, gasPrice, milesPerGallon)
-    resaleTotal = program2(cost, MILES_DRIVEN, gasPrice,  milesPerGallon, resaleValue, True)
 
-    print("Total cost for a 2020 Stock Ford Escape (not including resale): $%s" % baseTotal)
-    print("Total cost for a 2020 Stock Ford Escape (including resale): $%s" % resaleTotal)
-    
+    baseTotal = program2(cost, MILES_DRIVEN, gasPrice, milesPerGallon)
+    resaleTotal = program2(
+        cost, MILES_DRIVEN, gasPrice, milesPerGallon, resaleValue, True
+    )
+
+    print(
+        "Total cost for a 2020 Stock Ford Escape (not including resale): $%s"
+        % baseTotal
+    )
+    print(
+        "Total cost for a 2020 Stock Ford Escape (including resale): $%s" % resaleTotal
+    )
+
     # Program 3
     time1 = int(input("Please enter the first time:\n"))
     time2 = int(input("Please enter the second time:\n"))
 
     program3(time1, time2)
-
-    
