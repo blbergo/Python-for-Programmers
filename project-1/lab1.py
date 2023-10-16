c7644ff75872c64edd45574ad0cce229a5a65c66
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-10-16 13:46:59 -0700

    lab 1

--- project-1/lab1.py
+++ project-1/lab1.py
@@ -20,7 +20,7 @@ def execute_menu(choice, text):
         "c": get_num_of_non_WS_characters,  # number of non-whitespace chars,
         "w": get_num_of_words,  # number of words
         "f": fix_capitalization,  # fix capitalization,
-        "r": "",  # replace punctuation
+        "r": replace_punctuation,  # replace punctuation
         "s": shorten_space,  # shorten spaces
     }
 
@@ -29,17 +29,17 @@ def execute_menu(choice, text):
         choice = input()
 
 
-def get_num_of_non_WS_characters(text, *args):
+def get_num_of_non_WS_characters(text):
     text = text.replace(" ", "")
     print(f"Number of non-whitespace characters: {len(text)}")
 
 
-def get_num_of_words(text, *args):
+def get_num_of_words(text):
     text = text.split(" ")
     print(f"Number of words: {len(text)}")
 
 
-def fix_capitalization(text, *args):
+def fix_capitalization(text):
     # capitalize the first char
     capitalize = True
     count = 0
@@ -60,7 +60,20 @@ def fix_capitalization(text, *args):
     print(f"Number of letters capitalized: {count}")
     print(f"Edited text: {s}")
 
-    # ask about replace_punctuation
+def replace_punctuation(text):
+    exclamationCount = len(text.replace("!", ""))
+    semicolonCount = len(text.replace(";", ""))
+
+    text = text.replace("!", ".")
+    text = text.replace(";", ",")
+
+    print("Punctuation replaced")
+    print(f"Number of exclamation marks: {exclamationCount}")
+    print(f"Number of semicolons: {semicolonCount}")
+
+    print(f"Edited text: {text}")
+
+
 def shorten_space(text):
     text = text.replace("  ", " ")
     print(f"Edited text: {text}")
